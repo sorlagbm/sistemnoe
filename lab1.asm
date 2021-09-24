d909ebc40d1b2a1081a4846ded6fb11bcb09b1d9
sorlagbm <60257879+sorlagbm@users.noreply.github.com>
2021-09-25 01:03:18 +0300

    Update lab1.asm

--- lab1.asm
+++ lab1.asm
@@ -1,70 +1,66 @@
 section .data
 ; 1.1 
-a db 10
-b db 27
-c db 0
+    a db 10
+    b db 27
+    c db 0
 
 ;1.2
-sum db 0
-def db 0
-pro db 1
-md db 2
-del db 3
+    sum db 0
+    def db 0
+    pro1 db 1
+    pro2 db 1
+    md db 2
+    del db 3
 
 section .text
-global _start
+    global _start
 _start:
 
 ;1.3
-mov eax, a
-mov ebx, b
-add eax, ebx
-mov [sum], eax
+    mov eax, a
+    mov ebx, b
+    add eax, ebx
+    mov [sum], eax
 
 
 ;1.4
-mov eax, a
-mov ebx, b
-sub eax, ebx
-mov [def], eax
+    mov eax, a
+    mov ebx, b
+    sub eax, ebx
+    mov [def], eax
 
 ;1.5
-mov eax, a
-mov ebx, b
-neg ebx
-mov [b], ebx
-sub eax, ebx
-mov [def], eax
-
+    mov eax, a
+    mov ebx, b
+    neg ebx
+    mov [b], ebx
+    sub eax, ebx
+    mov [def], eax
+    
 ;1.6
-mov eax, a
-mov ebx, b
-imul eax, ebx
-mov [pro], eax
-;mul eax, ebx
-
+    mov eax, a
+    mov ebx, b
+    mul ebx
+    mov [pro1], eax
+    imul ebx
+    mov [pro2], eax
+    
 ;1.7
-mov eax, a
-mov ebx, b
-div ebx, eax
-mov [del], ebx
-mov [md], ah
+    mov eax, a
+    mov ebx, b
+    div ebx
+    mov [del], eax
+    mov [md], edx
 
 ;1.8
-mov eax, [del]
-bts eax, 7
-bts eax, 6
-not eax
-btr eax, 5
-btr eax, 6
-btr eax, 7
+    mov eax, [del]
+    bts eax, 7
+    bts eax, 6
+    not eax
+    btr eax, 5
+    btr eax, 6
+    btr eax, 7
 
 ;1.9
-mov ebx, eax
-xor ebx, eax 
-
-
-
-
-
-
+    mov ebx, eax
+    xor ebx, eax 
